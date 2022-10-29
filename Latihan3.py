fea2fa0e4999e64629e094cd988c2149e5aa0ee3
Midun2103 <midunhakiki@mhs.pelitabangsa.ac.id>
2022-10-30 00:30:38 +0700

    Praktikum3

---
+++ Latihan3.py
@@ -0,0 +1,8 @@
+#menampilkan output * berbentuk diamond
+inputData = eval(unput("Enter diamond's height: "))
+
+for i in range(inputData):
+    print(" " * (inputData - i), "*" * (2*i + 1))
+for i in range(inputData - 2, -1, -1):
+    print(" " * (inputData - i), "*" * (2*i + 1))
+ 
